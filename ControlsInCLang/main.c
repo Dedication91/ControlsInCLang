7e65e65de1c92676a5ee345a6d7f910feb6776c8
SM <smtechnologist91@users.noreply.github.com>
2017-07-09 05:15:31 -0700

    Final

---
+++ ControlsInCLang/main.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+
+    int powerVar = 500;
+    if (powerVar >= 1000) {
+        printf("Power is enought\n");
+    
+    } else if (powerVar == 5000) {
+        printf("Power is too much\n");
+    }
+    
+    int speedVar = 300;
+    if (powerVar > 600|| speedVar == 300) {
+        printf("Not bad!\n");
+    }
+
+
+    
+    
+    
+    
+    return 0;
+}
